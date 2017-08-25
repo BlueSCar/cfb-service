422b37b6f7e4c2ae409ec64ea1ec184fca8469d6
Bill Radjewski <radjewwj@gmail.com>
2017-08-25 18:13:35 -0400

    redis test

--- server.js
+++ server.js
@@ -8,7 +8,16 @@ try {
     let google = require('google');
     let googleAuth = require('google-auth-library');
 
-    
+    let redis = require('redis');
+    let client = redis.createClient();
+
+    client.sadd("gameIds", "game1");
+    client.sadd("gameIds", "game2");
+    client.sadd("gameIds", "game3");
+
+    Promise.fromCallback((cb) => client.smembers("gameIds", cb)).then((result) => {
+        console.log(result);
+    });
 
 } catch (err) {
     console.error(error);
