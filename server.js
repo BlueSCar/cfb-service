aeb3a174da99755936597e8e8fb43debcd3c4fc0
Bill Radjewski <BRADJEWS@assurexhealth.com>
2017-08-29 19:58:08 -0400

    Full implementation

--- server.js
+++ server.js
@@ -3,21 +3,20 @@ try {
 
     // depedencies
     let cfb = require('cfb-data');
+    let fs = require('fs');
+    let json2csv = require('json2csv');
     let RateLimiter = require('limiter').RateLimiter;
     let Promise = require('bluebird');
     let google = require('googleapis');
     let googleAuth = require('google-auth-library');
+    let schedule = require('node-schedule');
 
     let redis = require('redis');
     let redisClient = require('./lib/redis')(redis, Promise);
-
-    let driveClient = require('./lib/google')(RateLimiter, google, googleAuth, Promise, redisClient);
-    
+    let driveClient = require('./lib/google')(RateLimiter, google, googleAuth, Promise, redisClient, json2csv, fs);
     let gamesClient = require('./lib/games')(cfb, driveClient, redisClient);
 
-    redisClient.setYear(null);
-    redisClient.setWeek(null);
-    gamesClient.syncGames();
+    let job = schedule.scheduleJob("* * * * *", gamesClient.syncGames);
 
 } catch (err) {
     console.error(err);
